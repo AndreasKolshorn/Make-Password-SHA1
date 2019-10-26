bb06e178d8128b26a2da7feb58f300c27bc95819
Akolshorn <AndreasKol@users.noreply.github.com>
2019-10-25 17:36:50 -0700

    Initial project creation
    Create a C# .NET Core 3 console application. Convert a JS sample of SHA1 into C# class.

---
+++ Make-Password-SHA1/Make-Password-SHA1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Make_Password_SHA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
